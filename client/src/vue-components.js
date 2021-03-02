a461fb1a549457b8e930061cd697d7b82823d90b
Thomas Margosian <tmargo101@gmail.com>
2021-03-02 04:16:14 +0000

    - Removed console.logs

--- client/src/vue-components.js
+++ client/src/vue-components.js
@@ -3,10 +3,10 @@ Vue.component('nav-bar', {
   template:
   `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="#">
-<img src="/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
-{{title}}
-</a>
+  <a class="navbar-brand" href="#">
+    <img src="/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    {{title}}
+  </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
